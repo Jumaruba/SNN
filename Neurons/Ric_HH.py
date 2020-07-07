a365188b9f44f3090e46c8d2af71c12c11b7fd17
Ricardo <ricardo.nunes.1892@gmail.com>
2020-07-07 13:39:20 +0100

    Ric_HH is now more readble. Also, corrected an error there

--- Neurons/Ric_HH.py
+++ Neurons/Ric_HH.py
@@ -22,9 +22,9 @@ class Neuron:
         #auxiliary functions
         self.alphaN = lambda v : 0.01 * ( v + 10 ) / ( math.exp( ( v + 10) / 10 ) - 1 )
         self.alphaM = lambda v : 0.1 * ( v + 25 ) / ( math.exp( ( v + 25) / 10 ) - 1 )
-        self.alphaH = lambda v : 0.07 * math.exp(v / 20)
-        self.betaN = lambda v : 0.125 * math.exp(v / 80)
-        self.betaM = lambda v : 4 * math.exp(v / 18)
+        self.alphaH = lambda v : 0.07 * math.exp(v / 20.0)
+        self.betaN = lambda v : 0.125 * math.exp(v / 80.0)
+        self.betaM = lambda v : 4.0 * math.exp(v / 18)
         self.betaH = lambda v : 1 / (math.exp((v + 30)/ 10) + 1)
         
 
@@ -33,11 +33,14 @@ class Neuron:
 
     def stimulation(self):
         
-        n,m,h = 0,0,0
+        n,m,h = 1.0,1.0,1.0
         for t in range(1, self.steps):
+                
+            K = self.Gk * n**4 * (self.v[t-1] - self.Vk)
+            Na = self.GNa * m**3 * h * (self.v[t - 1] - self.VNa)
+            L = self.Gl * (self.v[t-1] - self.Vl)
             
-
-            dV = (self.i - self.Gk * n**4 * (self.v[t-1] - self.Vk) - self.GNa * m**3 * h * (self.v[t - 1] - self.VNa) + self.Gl * (self.v[t-1] - self.Vl)) * self.dt / self.Cm
+            dV = (self.i - K - Na - L) * self.dt / self.Cm
             self.v[t] = self.v[t-1] + dV
             
             dn = (self.alphaN(self.v[t]) * (1 - n) - self.betaN(self.v[t]) * n)*self.dt
