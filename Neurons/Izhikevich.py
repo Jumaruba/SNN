3f18e6d4f0a9822b180f7696aece2fb65a68b317
Jumaruba <juliane.marubayashi@gmail.com>
2020-07-08 17:37:04 +0100

    new graph for izikevitch

--- Neurons/Izhikevich.py
+++ Neurons/Izhikevich.py
@@ -5,45 +5,49 @@ import matplotlib.pyplot as plt
 
 class Neuron:
     def __init__(self):
-        self.tmax = 1000
-        self.dt = 0.5
-        self.steps = math.ceil(self.tmax / self.dt)
-        self.i = 10
-
         # setting parameters with the default value
         self.a = .02
         self.b = .2
         self.c = -65
         self.d = 8
 
+    def stimulation(self, time, I, dt):
+        steps = math.ceil(time / dt)
+
         # input
-        self.v = np.zeros(self.steps, dtype=float)
-        self.u = np.zeros(self.steps, dtype=float)
-        self.v[0] = -65  # resting potential
-        self.u[0] = -14
+        v = np.zeros(steps, dtype=float)
+        u = np.zeros(steps, dtype=float)
+        v[0] = -65  # resting potential
+        u[0] = -14
 
-    def stimulation(self):
-        for t in range(self.steps - 1):
-            if self.v[t] >= -30:  # spike
-                self.v[t + 1] = self.c
-                self.u[t + 1] = self.u[t] + self.d
+        for t in range(steps - 1):
+            if v[t] >= -30:  # spike
+                v[t + 1] = self.c
+                u[t + 1] = u[t] + self.d
             else:
-                dv = 0.04 * self.v[t] * self.v[t] + 5 * self.v[t] + 140 - self.u[t] + self.i
-                du = self.a * (self.b * self.v[t] - self.u[t])
-                self.u[t + 1] = self.u[t] + du
-                self.v[t + 1] = self.v[t] + dv
+                dv = (0.04 * v[t] * v[t] + 5 * v[t] + 140 - u[t] + I[t])
+                du = (self.a * (self.b * v[t] - u[t]))
+                u[t + 1] = u[t] + du
+                v[t + 1] = v[t] + dv
+        return v
 
-    def plot(self):
-        vTime = np.arange(0, 1000, 0.5, dtype=None)
-        plt.plot(vTime, self.v, color='b')
-        plt.plot(vTime, self.u, linestyle='dashdot')
-        plt.title("Single neuron stimulation")
-        plt.xlabel("Time [ms]")
-        plt.ylabel("Voltage [mv]")
-        plt.show()
+
+def plot(time, dt, v, I):
+    vTime = np.arange(0, time, dt, dtype=None)
+    plt.plot(vTime, v, color='b')
+    plt.plot(vTime, I, color='r')
+    plt.title("Single neuron stimulation")
+    plt.xlabel("Time [ms]")
+    plt.ylabel("Voltage [mv]")
+    plt.show()
 
 
 if __name__ == '__main__':
     n = Neuron()
-    n.stimulation()
-    n.plot()
+
+    time = 100
+    dt = 0.05
+    steps = math.ceil(time / dt)
+    I = [10 if (10 / 0.05 <= i <= 30 / 0.05 or 60 / 0.05 <= i <= 80 / 0.05) else 0 for i in range(steps)]
+    v = n.stimulation(time, I, dt)
+    plot(time, dt, v, I)
