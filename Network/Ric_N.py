bcc02b10b3aec92cbbb4bda558dd3789c5cee0a6
Ricardo <ricardo.nunes.1892@gmail.com>
2020-07-14 17:45:32 +0000

    Working on the network

--- Network/Ric_N.py
+++ Network/Ric_N.py
@@ -2,10 +2,11 @@ import numpy as np
 
 
 class Network:
-    def __init__(self, size, neuron):
+    def __init__(self):
+        """
         self.neuron = neuron
         self.size = size
-
+"""
     def fire(self):
         ne = 800
         ni = 200
@@ -21,12 +22,20 @@ class Network:
         firings = []                                            # Spike timings
 
         for t in range(1, 1001):                                                     # Simulation of 1000 ms
-            i = [5 * np.random.randn(ne, 1), 2 * np.random.randn(ni, 1)]            # Thalamic input
-            fired = np.array([i_v for i_v, vi in enumerate(v) if vi >= 30])                   # indices of spikes
-            firings = [firings,   t + 0*fired , fired]                                 # ????
-            v[fired] = c[fired]
-            u[fired] = u[fired] + d[fired]
-            i = i + sum(S[:, fired], 2)
+            i = [5 * np.random.randn(ne, 1), 2 * np.random.randn(ni, 1)]             # Thalamic input
+            print(v, "Break\n")
+            fired = v[v >= 30]                                                       # indices of spikes
+            firings.append(fired)                                                    # ????
+            print(fired)
+            if fired:
+                v[fired] = c[fired]
+                u[fired] = u[fired] + d[fired]
+                i = i + sum(S[:, fired], 2)
+
             v = v + 0.5 * (0.04 * v ** 2 + 5 * v + 140 - u + i)        # step 0.5ms
             v = v + 0.5 * (0.04 * v ** 2 + 5 * v + 140 - u + i)       # for numerical
             u = u + np.multiply(a, np.multiply(b, v) - u)             # stability
+
+
+net = Network()
+net.fire()
