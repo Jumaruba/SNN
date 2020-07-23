bfead183c8fc5c1fae3b934f7803c8af164a85ad
Jumaruba <juliane.marubayashi@gmail.com>
2020-07-23 15:30:03 +0100

    still having problems with sync

--- Network/Synapse.py
+++ Network/Synapse.py
@@ -12,7 +12,7 @@ El = -70			# [mV]
 rm_gs = 0.05			
 Rm_Ie = 25			# [mV]
 V_trhs = -54		# [mV]
-dt = 0.125			# [mS]  
+dt = 0.05			# [mS]  
 T = 100				# [mS] total time of analyses  
 Pmax = 1 
 V_reset = -80 
@@ -50,7 +50,7 @@ class LIF:
 				Ps_sum += self.Ps(self.time[i] - self.time[ti])
 
 		if self.v[i-1] > V_trhs: 
-			self.v[i-1] = 0
+			self.v[i-1] = 0			# setting last voltage to spike value 
 			self.v[i] = V_reset 
 		else: 
 			dv = (El - self.v[i-1] + Ps_sum* rm_gs * (self.v[i-1] - self.Es) + Rm_Ie)/tau_m*dt 
@@ -65,24 +65,27 @@ class LIF:
 if __name__ == '__main__':
 	steps = math.ceil(T/dt)
 	
-	n1 = LIF(T, dt) 
-	n2 = LIF(T, dt) 
+	n1 = LIF(T, dt, True) 
+	n2 = LIF(T, dt, True) 
 
 	neurons = [n1, n2]
 	n1.pre_neurons = [neurons[1]]
 	n2.pre_neurons = [neurons[0]] 
-
+	n1.v[0] = El + Rm_Ie
+	n2.v[0] = El + Rm_Ie
  
 
 	# spikes = dirac(t).timeV 
-	for i in range(steps): 
+	for i in range(1,steps): 
 		for neuron in neurons: 
 			if i == 0: 
 				neuron.v[0] = El
 			else:  
 				neuron.step(i) 
-
-	print(n1.dirac[:])
+	l1 = [n1.time[i] for i in range(steps) if n1.dirac[i] == 1]
+	l2 = [n1.time[i] for i in range(steps) if n2.dirac[i] == 1]
+	print(l1)
+	print(l2)
 	time = np.arange(0,T,dt)
 	plt.figure()
 	plt.subplot(3,1,1)
