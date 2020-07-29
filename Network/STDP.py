c50b3b9c518531432fa1d479359bdfa947dc94e1
Jumaruba <juliane.marubayashi@gmail.com>
2020-07-29 20:15:03 +0100

    finally fixed some problems: ps was being calculated wrongly

--- Network/STDP.py
+++ Network/STDP.py
@@ -17,16 +17,16 @@ global tau_m, El, rm_gs, Es, V_trhs, dt, T, Pmax
 tau_m = 20 			# [ms] how fast all the action happens  
 tau_s = 10 			# [ms] pyke 
 El = -70			# [mV]
-rm = 0.01			
-V_trhs = -54			# [mV]
+rm = 1
+V_trhs = -54		# [mV]
 dt = 0.05			# [mS]  
-T = 1000				# [mS] total time of analyses  
+T = 500			# [mS] total time of analyses  
 Pmax = 1 
 V_reset = -80 
 
 # STDP 
 gsMax = 0.01			# Max value to gs 
-forgetTime = 1000       # [mS]  might be necessary to change 
+forgetTime = 1000       # [mS]  might be necessary to change
 A_plus = 0.01 			
 A_neg = 0.005			
 tau_plus = 20			# [mS]
@@ -63,7 +63,7 @@ class Synapse:
 	
 	# Checks if the last spike was at the specific time given 
 	def previous_spike_time(self, time): 
-		if self.lenTime - 1 < 0 : return False 
+		if self.lenTime - 1 < 0: return False
 		return self.time[self.lenTime -1] == time
 
 
@@ -75,7 +75,7 @@ class Synapse:
 			if self.time[counter] <= time: 
 				spikes.append(self.time[counter])
 			counter -= 1 
-
+		
 		return spikes 
 
 '''LIF neuron with synapse implemented 
@@ -96,67 +96,77 @@ class LIF:
 	def __init__(self, T, dt,  In = False):
 
 	# setting constants
-		self.Es = -80 if In else 0  # [mV]	
+		self.Es = -80 if In else 0  				# [mV]
 		
-		self.Rm_Ie = 25			# [mV]
+		self.Rm_Ie = 100							# [mV]
 		self.steps = math.ceil(T/dt)
 		self.v = np.zeros(self.steps) 				# voltage historic  
 		self.v[0] = El 
 		self.pre_neuron	= []		 				# pre-synaptic neurons connected to it
 		self.actualTime = dt 						
-		
+		self.ps = 0
+
 		# STDP 
 		self.gs_list = []							# list weights of pre-sytnaptic connections 
 		self.gs = 0 
+		self.gs_ps = 0								# ps * gs
+
+
+		# to be deleted
 		self.gs_list2 = [0]
+		self.list_ps = [0] 
 
 		# Synapse  
 		self.max_spikes_anal = 10 					# max spikes to be considered by the algorithm to calculate Ps
 		self.synapse = Synapse(self.max_spikes_anal) 
 
 	def step(self, i):
-		self.actualTime += dt 
-		
-		Ps_sum = self.Ps_sum() 
-
-		if Ps_sum > Pmax : 
-			Ps_sum = Pmax 
+		self.actualTime += dt
 
 		if self.v[i-1] > V_trhs: 
 			self.v[i-1] = 0							# setting last voltage to spike value 
 			self.v[i] = V_reset 
 		else: 
-			self.rk4(Ps_sum, i)
+			self.euler(i)
 
 		if self.v[i] >= V_trhs: 
 			self.synapse.add_spike(self.actualTime) 
 
 		self.STDP()  								# udpate all the weights 
 		self.gs_list2.append(self.gs)
+		self.list_ps.append(self.ps)
 
 
 	def STDP(self): 
-		# update the value of gs for each neuron 
-		for i,neuron in enumerate(self.pre_neuron): 
-			gs = 0
+		# update the value of gs for each neuron
+		for i, neuron in enumerate(self.pre_neuron):
 			f_t = 0
 			spikes = neuron.synapse.get_spikes_delta_time(self.actualTime)
 			# for each spike in the range of time forgetTime, get f(delta_t)
-			for spike in spikes: 
+			for spike in spikes:
+				# calculates the f_t
 				delta_t = self.actualTime - spike
-				f_t += A_plus*np.exp(delta_t/tau_plus) if delta_t > 0 else  -A_neg*np.exp(delta_t/tau_neg) 
+				f_t += A_plus*np.exp(delta_t/tau_plus) if delta_t > 0 else -A_neg*np.exp(delta_t/tau_neg)
+
+			# calculates the ps
+			self.ps = self.Ps_sum(neuron)
+
 			self.gs_list[i] += gsMax*f_t
+
 			if self.gs_list[i] > 1: 
 				self.gs_list[i] = 1 
 
-		self.gs = sum(self.gs_list) 
 
 
-	def euler(self, Ps_sum, i):
-		dv = (El - self.v[i - 1] + Ps_sum * rm * self.gs * (self.v[i - 1] - self.Es) + self.Rm_Ie) / tau_m * dt
+		self.gs = sum(self.gs_list)
+		self.gs_ps = self.gs * self.ps
+
+
+	def euler(self, i):
+		dv = (El - self.v[i - 1] + rm * self.gs_ps * (self.v[i - 1] - self.Es) + self.Rm_Ie) / tau_m * dt
 		self.v[i] = dv + self.v[i - 1]
 
-
+	# to fix this one
 	def rk4(self, Ps_sum, i):
 
 		dv1 = self.fu(self.v[i - 1], Ps_sum) * dt
@@ -170,9 +180,9 @@ class LIF:
 	def fu(self, v, Ps_sum):
 		return (El - v + Ps_sum * rm * self.gs * (v - self.Es) + self.Rm_Ie) / tau_m
 
-	def Ps_sum(self):
+	def Ps_sum(self, neuron):
 		ps_sum = 0
-		for ti in self.synapse.time:
+		for ti in neuron.synapse.time:
 			ps_sum += self.Ps(self.actualTime - ti)
 		
 		return ps_sum 
@@ -200,7 +210,6 @@ class Network:
 					for pre_n in self.neurons[i - 1]:
 						neuron.add_pre_neuron(pre_n)
 
-
 				layer.append(neuron)     						# a remover parametros
 
 			self.neurons.append(layer)
@@ -220,16 +229,33 @@ n.run()
 
 time = np.arange(0, T, dt)
 plt.figure()
-plt.title("Voltage")
-plt.plot(time, n.neurons[2][0].v) 
+plt.title("Voltage neuron 0")
+plt.plot(time, n.neurons[1][0].v)
 plt.xlabel("t (ms)")
 plt.ylabel("V6 (mV)")
+
+
+time = np.arange(0, T, dt)
+plt.figure()
+plt.title("Voltage neuron 1 ")
+plt.plot(time, n.neurons[2][0].v)
+plt.xlabel("t (ms)")
+plt.ylabel("V3 (mV)")
+
+time = np.arange(0, T, dt)
+plt.figure()
+plt.title("ps")
+plt.plot(time, n.neurons[1][0].list_ps) 
+plt.xlabel("t (ms)")
+plt.ylabel("V (mV)")
 
 time = np.arange(0, T, dt)
 plt.figure()
 plt.title("gs")
-plt.plot(time, n.neurons[2][0].gs_list2) 
+plt.plot(time, n.neurons[1][0].gs_list2)
 plt.xlabel("t (ms)")
 plt.ylabel("V (mV)")
+
+
 
 plt.show()
