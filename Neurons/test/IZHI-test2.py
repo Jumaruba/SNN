eb9723432811c7a7adbd5d4d723287de93278eba
Jumaruba <juliane.marubayashi@gmail.com>
2020-08-01 16:28:50 +0100

    tests for izhikevich

---
+++ Neurons/test/IZHI-test2.py
@@ -0,0 +1,15 @@
+from __init__ import * 
+from plot import plot
+
+'''Test RUNGE KUTTA 4 ORDER METHOD
+
+There's unity test, it's necessary to compare the output image with the image from the folder expected-results
+'''
+
+neuron = Izhi() 
+time = 500
+dt = 0.01
+steps = math.ceil(time / dt)
+I = [0 if 200/dt <= i <= 300/dt  else 10 for i in range(steps)]
+
+plot(neuron, time, dt, I, 1)
---
+++ Neurons/test/__init__.py
@@ -0,0 +1,11 @@
+try: 
+    import sys 
+    import os 
+    import math 
+    import numpy as np 
+    import matplotlib.pyplot as plt
+    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    from IZHI import Izhi as Izhi
+
+except Exception as e: 
+    print("Some Modules are missing -- \n{}".format(e)) 
